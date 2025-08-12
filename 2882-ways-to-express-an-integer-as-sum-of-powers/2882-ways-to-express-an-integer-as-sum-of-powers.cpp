93ff46910c32342ca95c1f23a22dc65732710e29
Mirin Mano M <146096264+Mirinmano@users.noreply.github.com>
2025-08-12 11:07:54 +0530

    Time: 113 ms (65.58%), Space: 82.8 MB (41.83%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
+    const int MOD = 1e9 + 7;
     vector<vector<int>> dp;
-    const int MOD = 1e9+7;
+    vector<int> powers;
 
-    int helper(int n, int x, int i){   
-        if(n<0 || i<1) return 0;
-        if(n<=1) return 1;
+    int helper(int n, int i) {
+        if (n == 0) return 1;   // found a valid way
+        if (n < 0 || i < 1) return 0;
 
-        if(dp[n][i]!=-1) return dp[n][i];
+        if (dp[n][i] != -1) return dp[n][i];
 
-        long long t = helper(n-int(pow(i, x)), x, i-1);
-        long long nt = helper(n, x, i-1);
+        long long take = 0;
+        if (powers[i] <= n) take = helper(n - powers[i], i - 1);
+        long long skip = helper(n, i - 1);
 
-        return dp[n][i] = (t+nt)%MOD;
+        return dp[n][i] = (take + skip) % MOD;
     }
 
     int numberOfWays(int n, int x) {
-        int m = ceil(pow(n, 1.0/x));
-        dp.assign(n+1, vector<int>(m+1, -1));
-        return helper(n, x, m);
+        int m = ceil(pow(n, 1.0 / x)); 
+        dp.assign(n + 1, vector<int>(m + 1, -1));
+
+        powers.resize(m + 1);
+        for (int i = 1; i <= m; i++) {
+            long long p = 1;
+            for (int j = 0; j < x; j++) p *= i;
+            powers[i] = (int)p;
+        }
+
+        return helper(n, m);
     }
 };
